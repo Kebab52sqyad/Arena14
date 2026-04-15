b65f4926829821277a98bb7ca1dffb2a64464d58
sandmit <170251214+sandmit@users.noreply.github.com>
2026-04-15 20:53:05 +0300

    1

--- Corvax/Content.Corvax.Interfaces.Shared/obj/Release/net8.0/Content.Corvax.Interfaces.Shared.AssemblyInfo.cs
+++ Corvax/Content.Corvax.Interfaces.Shared/obj/Release/net8.0/Content.Corvax.Interfaces.Shared.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Content.Corvax.Interfaces.Shared")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f58a32d6de86346a7d9a2a334258ca694c009602")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7b86c7612d2a90e46d26698ae8e3265a3b80e5a4")]
 [assembly: System.Reflection.AssemblyProductAttribute("Content.Corvax.Interfaces.Shared")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Content.Corvax.Interfaces.Shared")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
